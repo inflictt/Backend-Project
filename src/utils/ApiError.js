f7731c0e2374a31b8ee4479b5d94ea78f8cb04f9
Saksham Lodha <legitsaksham@gmail.com>
2026-05-24 23:23:09 +0530

    utils and api error handling async  classes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,22 +1,30 @@
 class ApiError extends Error {
+
     constructor(
-        statusCode, 
-        errors =[],
+        statusCode,
         message = "Something went wrong",
-        stack="",)
-        {
-        super(message)
-        this.statusCode = statusCode
-        this.data = null
-        this.message = message
+        errors = [],
+        stack = ""
+    ) {
+
+        super(message);
+
+        this.statusCode = statusCode;
+        this.data = null;
+        this.message = message;
         this.success = false;
-        this.errors = errors
-        if (stack){
-            this.stack = stack
-        }else{
-            Error.captureStackTrace(this,this.constructor)
+        this.errors = errors;
+
+        if (stack) {
+
+            this.stack = stack;
+
+        } else {
+
+            Error.captureStackTrace(this, this.constructor);
+
         }
     }
 }
 
-module.exports = ApiError
+export default ApiError;
